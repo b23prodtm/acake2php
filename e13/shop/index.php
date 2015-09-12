c9e44a78bec3a7d37fa1ce440c13c9a7499185a2
b23production GNU <b23prodtm@users.sourceforge.net>
2015-09-12 12:52:08 +0200

    index web shop commented etc/ site map files

---
+++ e13/shop/index.php
@@ -0,0 +1,22 @@
+<?php
+$nouvelleAdresse='https://store.esellerate.net/descom/store';	//Nouvelle adresse
+if (filter_input(INPUT_SERVER,'QUERY_STRING')&&(filter_input(INPUT_SERVER,'QUERY_STRING')!=''))
+ $nouvelleAdresse.='?'.substr(filter_input(INPUT_SERVER,'QUERY_STRING'),0,2048);	//Récupère les paramètres
+header('Location: '.$nouvelleAdresse);	//Redirection HTTP
+header('HTTP/1.1 301 Moved Permanently');
+header('Status: 301 Moved Permanently');
+header('Content-Type: text/html; charset=UTF-8');
+$nouvelleAdresse=htmlspecialchars($nouvelleAdresse,ENT_QUOTES);	//Encode les caractères HTML spéciaux
+echo '<!DOCTYPE html>'."\n",
+ '<html xmlns="http://www.w3.org/1999/xhtml">'."\n",
+ '<head>'."\n",
+ '<meta charset="UTF-8" />'."\n",
+ '<meta http-equiv="refresh" content="0; url='.$nouvelleAdresse.'" />'."\n",	//Redirection HTML
+ '<title>Redirection</title>'."\n",
+ '<meta name="robots" content="noindex" />'."\n",
+ '</head>'."\n",
+ "\n",
+ '<body>'."\n",
+ '<p><a href="'.$nouvelleAdresse.'">Redirection</a></p>'."\n",
+ '</body>'."\n",
+ '</html>'."\n";
