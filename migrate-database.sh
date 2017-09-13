6be4b946c723b2e66418f2af0336e78f7c3559cb
b23production GNU <b23prodtm@users.sourceforge.net>
2017-09-13 15:03:21 +0200

    lib cake console cake

--- migrate-database.sh
+++ migrate-database.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ ! -f app/Config/Schema/schema.php ]; then
 	echo "Generating database schema 'cake schema generate' ..."
-	cake schema generate
+	./lib/Cake/Console/cake schema generate
 fi
 
 echo "Migrating database 'cake schema create' ..."
-cake schema create
+./lib/Cake/Console/cake schema create
