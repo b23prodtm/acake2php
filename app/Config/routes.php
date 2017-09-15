9c4465b5ca4bca2f00603bae1ad700d5752d89e0
b23production GNU <b23prodtm@users.sourceforge.net>
2017-09-15 15:24:48 +0200

    routing w/ defaults

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -34,7 +34,12 @@
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
-	Router::connect('/e14/:action/*', array('controller' => 'e14', 'action' => 'index'));
+	Router::connect('/e14/:action/**', array('controller' => 'e14', 'action' => 'index'));
+        /**
+ * ...and connect the rest of 'Pages' controller's URLs.
+ */
+	Router::connect('/e14/*', array('controller' => 'e14', 'action' => 'index'));
+
 /**
  * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
@@ -44,5 +49,6 @@
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
  * the built-in default routes.
- */
+ *
 	require CAKE . 'Config' . DS . 'routes.php';
+ */
--- app/View/E14/dvd.ctp
+++ app/View/E14/dvd.ctp
@@ -1,8 +1,9 @@
 <?php
                 if (stristr($p, ".php")) {
-                        include($GLOBALS["dvd"] . DS  . $p);
+                        include($GLOBALS["library"] . DS  . $p);
                 } else if ($p) {
-                        include($GLOBALS["dvd__" . $p]);
+                        include($GLOBALS["library__" . $p]);
                 } else {
-                        include($GLOBALS["dvd__index"]);
+                        include($GLOBALS["library__index"]);
                 }
+?>
