9ee0bd575513e0af3404b48bd437171400615e1c
b23production GNU <b23prodtm@gmail.com>
2017-09-18 01:20:28 +0200

    routes

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -15,22 +15,24 @@
  * Here, we are connecting '/' (base path) to controller called 'Pages',
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
- */
+ *
 	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+ */
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
-        /**
- * ...and connect the rest of 'Pages' controller's URLs.
- */
-	Router::connect('/e14/', array('controller' => 'e14', 'action' => 'index'));
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
      */
-	Router::connect('/e14/:action/**', array('controller' => 'e14'));
+	Router::connect('/e14/', array('controller' => 'e14', 'action' => 'index'));
 
+/**
+ * ...and connect the rest of 'Pages' controller's URLs.
+ */
+	Router::connect('/e14/:action/**', array('controller' => 'e14'));
+   
 /**
  * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
@@ -40,5 +42,6 @@
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
  * the built-in default routes.
- */
+ *
 	require CAKE . 'Config' . DS . 'routes.php';
+*/
