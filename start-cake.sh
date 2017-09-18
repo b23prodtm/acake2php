f05bcd0fe999f702d4652c2055122e720583bf9e
b23production GNU <b23prodtm@users.sourceforge.net>
2017-09-18 23:12:58 +0200

    database.php test and production configuration development server script

--- start-cake.sh
+++ start-cake.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-lib/Cake/Console/cake server -p 8080
+export DATABASE_ENGINE="mysql"
+export DATABASE_SERVICE_NAME="mysql"
+export TEST_MYSQL_SERVICE_HOST="localhost"
+export TEST_MYSQL_SERVICE_PORT="3306"
+export TEST_DATABASE_NAME="phpcms"
+export TEST_DATABASE_USER="test"
+export TEST_DATABASE_PASSWORD="mypassword"
+export FTP_SERVICE_HOST="local"
+export FTP_SERVICE_USER="test"
+export FTP_SERVICE_PASSWORD="mypassword"
+export GET_HASH_PASSWORD="saINNH2X5e87I"
+# local default configuration deployment
+cp app/webroot/php-cms/e13/etc/constantes_template.properties app/webroot/php-cms/e13/etc/constantes_local.properties
+
+# Know-How : In Openshift 3, configure a CakePhp-Mysql-persistent docker image. Disable automatic image stream deployment.
+# Be sure that lib/Cake/Console/cake test app and Health checks should return gracefullly, or the pods get terminated after a short time.
+# Scale down to zero, start build and deploy when finished to build. Then scale up to your pod usage needs.
+
+lib/Cake/Console/cake server -p 2233
