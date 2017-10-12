7065b453de7c47e5bef4427fb730dbfd74c80f0f
b23production GNU <b23prodtm@users.sourceforge.net>
2017-10-12 23:26:47 +0200

    workaround to fix github issue #9
    Install.sh is improved to define etc/constantes_template.properties (makes backup before templating)
    The shell configures a password with etc/getHashPassword.php
    migrate_database.sh -Y -Y -Y runs without user prompt
    start_cake.sh is commented

--- install.sh
+++ install.sh
@@ -1,26 +1,72 @@
 #!/bin/sh
 cd app/webroot/php-cms/e13/etc/
+copies=0
 while true
 do
         # (1) prompt user, and read command line argument
-        echo "It will overwrite constantes.properties. (Y/N) ?"
-        read -p "Run the shell script now ? " answer
+        echo "It will overwrite constantes.properties."
+        answer=$1
+        case $answer in
+               -[yY]* )
+                        answer="Y";;
+        
+               -[nN]* ) break;;
 
+               * )
+                        read -p "Run the shell script now (Y/N) ? " answer;;
+        esac
+        while [ -f constantes.properties.old-$copies ] || [ -f constantes_local.properties.old-$copies ]
+        do
+        let copies++
+        done
         # (2) handle the input we were given
         case $answer in
-                [yY]* ) 
-                        cp -v constantes.properties constantes.properties.old
+                [yY]* )                         
+                        cp -v constantes.properties constantes.properties.old-$copies
                         cp -v constantes_template.properties constantes.properties
-                        cp -v constantes_template.properties constantes_local.properties.old
+                        cp -v constantes_template.properties constantes_local.properties.old-$copies
                         cp -v constantes_template.properties constantes_local.properties
                         echo "Okay, just ran the shell script. Please, review the files."
+                        #quit while loop
                         break;;
 
-                [nN]* ) exit;;
+                [nN]* ) break;;
 
                 * )     echo "Dude, just enter Y or N, please.";;
-  esac
+        esac
 done
+#; get hash password argv are -p password -s salt
+pass=$2
+salt=$4
+#; read password if not set as $3 argv
+case $pass in
+        -[pP]* ) pass=$3;;
+        
+        * )
+                while true 
+                do
+                        read -p "Please enter a password :" pass
+                        read -p "Please re-enter the password :" confirmpass
+                        if [ "$pass" == "$confirmpass" ]; then
+                                break
+                        else
+                                echo "Passwords don't match."
+                        fi
+                done;;
+esac
+# read salt if not set as $5 argv
+case $salt in
+        -[sS]* ) salt=$5;;
+
+        *)
+                while [$salt == ""] 
+                do
+                let read -p "Please enter the 'salt' word :" salt 
+                done;;
+esac
+                         
+php -f getHashPassword.php -- -p $pass -s $salt
+
 cd ../../../../../
 
 # Know-How : In Openshift 3, configure a CakePhp-Mysql-persistent docker image. Disable automatic image stream deployment.
--- start-cake.sh
+++ start-cake.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#;
+#;
+#; this is configuration for development phase and runtime
+#;
+#;
 export DATABASE_ENGINE="mysql"
 export DATABASE_SERVICE_NAME="mysql"
 export TEST_MYSQL_SERVICE_HOST="localhost"
@@ -9,7 +14,17 @@ export TEST_DATABASE_PASSWORD="mypassword"
 export FTP_SERVICE_HOST="local"
 export FTP_SERVICE_USER="test"
 export FTP_SERVICE_PASSWORD="mypassword"
+#;
+#;
+#; this is generated for the development phase, don't use the same values for production !
+#;
+#;
 export GET_HASH_PASSWORD="saINNH2X5e87I"
+#;
+#;
+#; this development phase passwords, don't use the same values for production !
+#;
+#;
 export CAKEPHP_DEBUG_LEVEL=2
 
 echo "WELCOME HOMEPAGE http://localhost:8080/pages/home?local=1"
