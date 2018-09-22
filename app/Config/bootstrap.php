5dbb043f8fa8881b6552e8f861313a6185b5340b
Mac Bok Arimana <b23prodtm@users.sourceforge.net>
2018-09-22 17:38:23 +0200

    Coverity code scanner

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -118,4 +118,5 @@ CakePlugin::load('DebugKit');
 /**
  * Load Markdown Plugin
  */
-CakePlugin::load(array('Markdown' => array('bootstrap' => true)));
+CakePlugin::load(array('Markdown' => array('bootstrap' => true)));require_once 'vendors/autoload.php';
+require_once 'vendors/autoload.php';
