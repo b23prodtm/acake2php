02bdd74c814135ca69ce2cf01caa3f8d10409d9c
Mac Bok Arimana <b23prodtm@users.sourceforge.net>
2018-12-02 12:26:17 +0100

    ./configure.sh "-c" "-h" "-p" "pass" "-s" "word" "--mig-database" "-y";

--- configure.sh
+++ configure.sh
@@ -12,11 +12,11 @@ fi
 #; if the full set of the arguments exists, there won't be any prompt in the shell
 while [[ "$#" > 0 ]]; do case $1 in
     -[cC]*|--const)
-        shell_prompt "./Scripts/config_etc_const.sh" "${cyan}Step 1. Overwrite constantes.properties\n${nc}" '-Y'
+        shell_prompt "./Scripts/config_etc_const.sh" "${cyan}Step 1. Overwrite constantes.properties\n${nc}" "-Y"
         ;;
     -[hH]*|--hash)
     #; get hash password
-        shell_prompt "./Scripts/config_etc_pass.sh ${args}" "${cyan}Step 2. Get a hashed password with encryption, PHP encrypts.\n${nc}" '-Y'
+        shell_prompt "./Scripts/config_etc_pass.sh $*" "${cyan}Step 2. Get a hashed password with encryption, PHP encrypts.\n${nc}" "-Y"
         ;;
     -[dD]*|--mig-database)
         #; Know-How : In Openshift 3, configure a CakePhp-Mysql-persistent docker image. Set automatic deployment with _100%_ unavailability
@@ -24,7 +24,7 @@ while [[ "$#" > 0 ]]; do case $1 in
         #; Be sure that lib/Cake/Console/cake test app and Health checks should return gracefullly, or the pods get terminated after a short time.
         #; [[-d|--mig-database] [-uyiohn]] argument fixes up : Error: Database connection "Mysql" is missing, or could not be created.
         shift
-        args="$@"
+        args="$*"
         if [[ $openshift -eq 1 ]]; then args="${args} --openshift"; fi
         shell_prompt "migrate-database.sh $args" "${cyan}Step 3. Migrate database\n${nc}" '-Y'
         ;;
