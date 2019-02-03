52cbf603989c6610bb0b6e045974314005084155
b23production GNU <b23prodtm@users.sourceforge.net>
2019-02-03 23:11:05 +0100

    Update travis php.ini

--- Scripts/lib/locate.sh
+++ Scripts/lib/locate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 _locate() {
   [ $# -lt 1 ] && echo "Usage: $0 <filename>" && return $FALSE
-  return find /usr -name $1 | grep -m 1 $1
+  find /usr -name $1 | grep -m 1 $1
 }
 # export -f _locate
