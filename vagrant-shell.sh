08e9834f548482f8156363a3630719253069c286
Tiana Rakoto Arimanana <5566338+b23prodtm@users.noreply.github.com>
2020-04-19 22:00:15 +0200

    Fixes: PHP:  syntax error, unexpected '=' in /etc/php/7.2/cli/conf.d/0-session.save_path.ini

--- vagrant-shell.sh
+++ vagrant-shell.sh
@@ -8,7 +8,8 @@ if [[ "$OSTYPE" != "linux"* ]]; then
         exit 1
     fi
 fi
-REV=https://raw.githubusercontent.com/b23prodtm/vagrant-shell-scripts/b23prodtm-patch/ubuntu.sh
+#REV=https://raw.githubusercontent.com/b23prodtm/vagrant-shell-scripts/b23prodtm-patch/ubuntu.sh
+REV=https://raw.githubusercontent.com/b23prodtm/vagrant-shell-scripts/02d03f26d59020bbe8d3c827eff1174c2729c82d/ubuntu.sh
 LNK=/usr/local/share/vagrant-shell-scripts/ubuntu.sh
 mkdir -p $(dirname $LNK)
 curl -sSL $REV -o $LNK
