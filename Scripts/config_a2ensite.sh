63c9ec8083ce026e7a8d14ca378a41e945ff121a
Tiana Rakoto Arimanana <5566338+b23prodtm@users.noreply.github.com>
2020-07-10 23:27:21 +0200

    Fixes: ServerRoot

--- Scripts/config_a2ensite.sh
+++ Scripts/config_a2ensite.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -eu
+. init_functions .
+log_daemon_msg "Add VirtualHost $HTTPD_LISTEN"
 CNF="/etc/apache2"
-# DO
-WWW="${1:-/var/www/localhost/htdocs}"
+WWW="${1:-localhost/htdocs}"
 mkdir -p "$(dirname "$CNF")"
 mkdir -p "$(dirname "$WWW")"
 touch site.conf
@@ -22,12 +23,41 @@ echo -e "
         AllowOverride All
         Require all granted
     </Directory>
-    ErrorLog log/error.${SERVER_NAME}.log
-    CustomLog log/access.${SERVER_NAME}.log combined
+    ErrorLog logs/error.${SERVER_NAME}.log
+    TransferLog logs/access.${SERVER_NAME}.log
 </VirtualHost>
 ServerName ${SERVER_NAME}
 " >> site.conf
 cat site.conf
 mv site.conf "${CNF}/conf.d/"
+log_daemon_msg "SSL VirtualHost"
+touch ssl_site.conf
+echo -e "
+<VirtualHost ${HTTPD_LISTEN/:80/:443}>
+    DocumentRoot ${WWW}
+    ServerAdmin webmaster@${SERVER_NAME}
+    ServerName ${SERVER_NAME}:443
+    ServerAlias www.${SERVER_NAME}:443
+    <Directory \"${WWW}\">
+        DirectoryIndex index.php
+        Options +FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+    ErrorLog logs/ssl_error.${SERVER_NAME}.log
+    TransferLog logs/ssl_access.${SERVER_NAME}.log
+    SSLEngine on
+    SSLCertificateFile /etc/ssl/apache2/server.pem
+    SSLCertificateKeyFile /etc/ssl/apache2/server.key
+    #SSLCertificateChainFile /etc/ssl/apache2/server-ca.pem
+    #SSLCACertificatePath /etc/ssl/apache2/ssl.crt
+    #SSLCACertificateFile /etc/ssl/apache2/ssl.crt/ca-bundle.pem
+</VirtualHost>
+" >> ssl_site.conf
+cat ssl_site.conf
+mv ssl_site.conf "${CNF}/conf.d/"
+log_daemon_msg "Enable mod_rewrite"
 sed -i.old -E -e "/mod_rewrite.so/s/^#+//g" "${CNF}/httpd.conf"
 grep mod_rewrite.so < "${CNF}/httpd.conf"
+log_daemon_msg "Add www hosts $SERVER_NAME"
+sed -i "/127.0.0.1/s/(localhost)/\\1 ${SERVER_NAME} www.${SERVER_NAME}/" /etc/hosts
