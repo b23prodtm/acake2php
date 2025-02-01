557ce3973ce38e78a84e8df351a8149d68933421
add-MAINTAINER-email-to-environment <add-MAINTAINER-email-to-environment@github.com>
2025-02-01 20:36:44 +0100

    Added balena-storage container

---
+++ balena-storage/scripts/backup-db.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+STORAGE="$(find /db-backup -name 'storage_*$DEVNAME*' | head -n 1)"
+printf "Storage found at %s...\n" "$STORAGE"
+printf "Run shell command mysqldump...\n"
+export MYSQL_PWD="${MYSQL_ROOT_PASSWORD:-mariadb}"
+DUMP_FILE="$STORAGE/backup_$(date +%F_%H-%M-%S).sql"
+ERROR_LOG="$STORAGE/error.log"
+mysqldump -h db -u root --all-databases > "$DUMP_FILE" 2>> "$ERROR_LOG"
+printf "Delete dumps older than 6 months..."
+find /db-backup -type f -name "backup_*.sql" -mmin +259200 -exec rm -f {}
+#Cold backup works on balenaCloud only
+CONTAINER_NAME=$(balena ps | grep db | xargs printf "%s\n" "$1" | head -n 1)
+printf "Making full backup of databases container %s...\n" "$CONTAINER_NAME"
+balena cp "$CONTAINER_NAME:/config" "$STORAGE/db-data_$(date +%F_%H-%M-%S)"
+printf "Delete backups older than 1 month..."
+find /db-backup -type f -name "db-data_*" -mmin +64800 -exec rm -f {}
