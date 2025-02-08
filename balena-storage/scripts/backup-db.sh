35c2ec0eab648956bead0a675b23274815f873d6
Tina B Rakoto <webmaster@b23prodtm.info>
2025-02-08 15:41:00 +0100

    docker-compose

--- balena-storage/scripts/backup-db.sh
+++ balena-storage/scripts/backup-db.sh
@@ -8,7 +8,9 @@ ERROR_LOG="$STORAGE/error.log"
 mysqldump -h db -u root --all-databases > "$DUMP_FILE" 2>> "$ERROR_LOG"
 printf "Delete dumps older than 6 months..."
 find "$STORAGE" -type f -name "backup_*.sql" -mmin +259200 -exec rm -f {} \;
+
 #Cold backup works on balenaCloud only
+if [ ! $(command -v balena) > /dev/null ]; then printf "balena-cli was not installed, cannot perform cold backup.\n"; exit 0; fi
 CONTAINER_NAME=$(balena ps | grep db | xargs printf "%s\n" "$1" | head -n 1)
 printf "Making full backup of databases container %s...\n" "$CONTAINER_NAME"
 balena cp "$CONTAINER_NAME:/config" "$STORAGE/db-data_$(date +%F_%H-%M-%S)"
