c4309b5f209151c1f6424fb669ce271558a65448
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2025-04-20 17:35:11 +0200

    Fix error Cake patches (#109)
    * Fix error Cake patches
    
    * patches
    
    * composer scripts
    
    * Patches path

--- Scripts/composer.sh
+++ Scripts/composer.sh
@@ -14,10 +14,12 @@ if [ -n "$(command -v composer)" ]; then
         composer="composer"
 elif [ ! -f $composer ]; then
         slogger -st "$0" "Composer setup...\n"
-        mkdir -p bin
-        cd bin || exit 1
-        curl -sS https://getcomposer.org/installer | php
-        cd ..
+	mkdir -p $(dirname "$composer")
+	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+	php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
+	php composer-setup.php
+	php -r "unlink('composer-setup.php');"
+	mv -v composer.phar "$composer"
 fi
 # shellcheck disable=SC2154
 slogger -st "$0" "Composer ${green}[OK]${nc}"
