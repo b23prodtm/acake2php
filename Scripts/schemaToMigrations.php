4c5bf03a98ee3ba2f0982f5830d0283f71dae5c3
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2025-04-21 21:12:54 +0200

    Update schemaToMigrations.php

--- Scripts/schemaToMigrations.php
+++ Scripts/schemaToMigrations.php
@@ -1,8 +1,4 @@
 <?php
 require_once __DIR__."/../app/config/Schema/CakeSchema.php";
-if($argc < 2) print "Usage: __FILE__ <file.php>\n";
-else {
-	for($i=1; $i < count($argv); $i++) {
-		Config\Schema\CakeSchema::main(__DIR__."/".$argv[$i]);
-	}
-}
+use Config\Schema
+CakeSchema::main();
