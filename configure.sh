0a3b647926b1fcf64c999bd872c207d8d738b088
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2025-10-14 01:02:25 +0200

    Update configure.sh (#128)

--- configure.sh
+++ configure.sh
@@ -71,7 +71,7 @@ while [[ "$#" -gt 0 ]]; do case $1 in
     # shellcheck disable=SC2154
     echo -e "${green}Adding VirtualHost...${nc}"
     # shellcheck source=Scripts/config_a2ensite.sh
-    bash -c "$TOPDIR/Scripts/config_a2ensite.sh $TOPDIR/app/webroot $TOPDIR/etc/apache2"
+    bash -c "$TOPDIR/Scripts/config_a2ensite.sh $TOPDIR/app/webroot /etc/apache2"
     ;;
   -[vV]*|--verbose )
     set -x
