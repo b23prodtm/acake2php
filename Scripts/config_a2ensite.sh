38226add9623fed0e205e69481c5b4d0435e1a2b
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2025-10-14 16:55:14 +0200

    x86_64 pushed ./deployment/images/apache-php7

--- Scripts/config_a2ensite.sh
+++ Scripts/config_a2ensite.sh
@@ -5,8 +5,6 @@ TOPDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 export WWW="${1:-$TOPDIR/app/webroot}"
 export CNF="${2:-$TOPDIR/etc/apache2}"
 log_daemon_msg "Add VirtualHost $HTTPD_LISTEN ${WWW} to ${CNF}/conf.d/site.conf, ${BASH_SOURCE[0]} [www_directory:app/webroot] [site.conf_directory:/etc/apache2]"
-mkdir -p "$CNF/conf.d"
-mkdir -p "$WWW"
 envsubst < "${CNF}/site.tpl" > "${CNF}/conf.d/site.conf"
 log_daemon_msg "SSL VirtualHost"
 envsubst < "${CNF}/ssl_site.tpl" > "${CNF}/conf.d/ssl_site.conf"
@@ -15,7 +13,7 @@ if [ -f "${CNF}/httpd.conf" ]; then
   sed -i.old -E -e "/mod_rewrite.so/s/^#+//g" "${CNF}/httpd.conf"
   grep mod_rewrite.so < "${CNF}/httpd.conf"
 else
-  log_warning_msg "${CNF}/httpd.conf file not found"
+  log_warning_msg "APACHE2 SERVER CONFIG: ${CNF}/httpd.conf file not found, check second arg $0 $WWW $CNF"
 fi
 log_daemon_msg "Add /etc/hosts $SERVER_NAME"
 if [ -w "/etc/hosts" ]; then
