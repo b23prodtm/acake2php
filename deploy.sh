ec873401d0d6b37b10d2d8b8ffa7bf7ec2e69095
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2025-10-14 12:12:30 +0200

    Fix log message

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ DOCKER_USER="${DOCKER_USER:-betothreeprod}" COLUMNS=0 LINES=0 SYSTEMD_NO_WRAP=0 
 git stash
 balena_deploy "${BASH_SOURCE[0]}" "$@"
 git stash pop && git stash
+. init_functions .
 log_daemon_msg "Use  git stash apply && git commit -a  to commit the last deployment settings"
