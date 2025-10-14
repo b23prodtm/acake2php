241ec0048edac2a28ede010a4f8a9b65a7fd44e4
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2025-10-14 14:18:21 +0200

    x86_64 pushed ./deployment/images/apache-php7

--- Scripts/update-templates.sh
+++ Scripts/update-templates.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-ARC=(armhf x86_64 aarch64)
-# Fixes: not inside a working tree
-REV=$(git -C .git rev-parse 2>/dev/null)
+ARC=(x86_64 aarch64 armhf)
 # Loop through the array and echo each value
 for arch in "${ARC[@]}"; do
   printf "Updating templates, %s \n" "$arch"
   ./deploy.sh "$arch" 1 0 0 2> /dev/null > /dev/null
 done
-[[ "$REV" -eq 0 ]] && git add docker-compose.yml
-[[ "$REV" -eq 0 ]] && git commit -m "Updated Templates"
