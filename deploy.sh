d44aaa2904d80bb15074db594aea24f7f724a8da
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2025-10-14 12:22:58 +0200

    Better save deploy files (#140)
    * Better save deploy files
    
    * Fix log message

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@
 rm -f deployment/images/mysqldb/conf.d/custom.cnf
 # Fixes: unbound variables on ubuntu
 DOCKER_USER="${DOCKER_USER:-betothreeprod}" COLUMNS=0 LINES=0 SYSTEMD_NO_WRAP=0 \
+git stash
 balena_deploy "${BASH_SOURCE[0]}" "$@"
+git stash pop
