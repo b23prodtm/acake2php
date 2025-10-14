9d86f3cc84e0c691830c3cced6383aea6c75db79
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2025-10-14 03:54:11 +0200

    Update composer.sh

--- Scripts/composer.sh
+++ Scripts/composer.sh
@@ -15,7 +15,7 @@ elif [ ! -f $composer ]; then
 	mkdir -p "$(dirname "$composer")"
 	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 	php -r "if (hash_file('sha384', 'composer-setup.php') === 'ed0feb545ba87161262f2d45a633e34f591ebb3381f2e0063c345ebea4d228dd0043083717770234ec00c5a9f9593792') { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
-    php composer-setup.php
+  php composer-setup.php
 	php -r "unlink('composer-setup.php');"
 	mv -v composer.phar "$composer"
 fi
