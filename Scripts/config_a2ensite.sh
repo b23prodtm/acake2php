f1293c9ca1a9813752e0264fbd1a35ba43efd3ad
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2025-10-15 00:07:51 +0200

    Update config_a2ensite.sh

--- Scripts/config_a2ensite.sh
+++ Scripts/config_a2ensite.sh
@@ -4,7 +4,7 @@ TOPDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 . init_functions .
 WWW="${1:-$TOPDIR/app/webroot}"
 CNF="${2:-/etc/apache2}"
-log_daemon_msg "Shell call : $0 $@ ${WWW} host ${CNF}/conf.d/site.conf}"
+log_daemon_msg "Shell call : $0 $* ${WWW} host ${CNF}/conf.d/site.conf}"
 envsubst < "${CNF}/site.tpl" > "${CNF}/conf.d/site.conf"
 log_daemon_msg "SSL VirtualHost (not set)"
 envsubst < "${CNF}/ssl_site.tpl" > "${CNF}/conf.d/ssl_site.conf"
