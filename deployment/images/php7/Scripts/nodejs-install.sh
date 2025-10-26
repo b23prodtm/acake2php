0af5d35d830d4c1c4c8f6a07f6586252169dcd5e
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2025-10-27 00:28:22 +0100

    x86_64 pushed ./deployment/images/php7

--- deployment/images/php7/Scripts/nodejs-install.sh
+++ deployment/images/php7/Scripts/nodejs-install.sh
@@ -9,7 +9,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 
 # Download and install Node.js:
-nvm install 18
+nvm install "${NODE_VERSION:-18}"
 
 # Verify the Node.js version:
 node -v # Should print "v18.20.8".
