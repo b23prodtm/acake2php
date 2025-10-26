0d97feb94271848ebaa33343b0c2c90279b1756e
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2025-10-26 19:32:39 +0100

    node

--- Scripts/nodejs-install.sh
+++ Scripts/nodejs-install.sh
@@ -2,7 +2,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 # in lieu of restarting the shell
-\. "$HOME/.nvm/nvm.sh"
+\. "~/.nvm/nvm.sh"
 
 # Download and install Node.js:
 nvm install 18
