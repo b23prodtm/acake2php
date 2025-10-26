84ff4c3bbff416054c07ea2f243d9d139d88b25c
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2025-10-26 23:16:23 +0100

    node

---
+++ deployment/images/php7/Scripts/nodejs-install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+# shellcheck disable=SC1090
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 18
+
+# Verify the Node.js version:
+node -v # Should print "v18.20.8".
+
+# Download and install Yarn:
+corepack enable yarn
+
+# Verify Yarn version:
+yarn -v
+
