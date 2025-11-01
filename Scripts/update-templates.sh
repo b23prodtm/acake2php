b2214e219f24166b7bbc4056011ae19850c94ac9
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2025-11-01 14:17:52 +0100

    reset commit pr

--- Scripts/update-templates.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-ARC=(x86_64 aarch64 armhf)
-# Loop through the array and echo each value
-for arch in "${ARC[@]}"; do
-  printf "Updating templates, %s \n" "$arch"
-  ./deploy.sh "$arch" 3 0 2> /dev/null > /dev/null
-done
