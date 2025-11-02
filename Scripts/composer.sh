2312b88b59b8c395b81836ff27008efaf9020336
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2025-11-02 18:01:07 +0100

    FPM Docker Compose HTTPD (#155)

--- Scripts/composer.sh
+++ Scripts/composer.sh
@@ -7,17 +7,13 @@
 TOPDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 # shellcheck source=lib/logging.sh
 . "${TOPDIR}/Scripts/lib/logging.sh"
-composer="bin/composer"
+composer="${TOPDIR}/bin/composer"
 if [ -n "$(command -v composer)" ]; then
         composer="composer"
 elif [ ! -f $composer ]; then
         slogger -st "$0" "Composer setup...\n"
 	mkdir -p "$(dirname "$composer")"
-	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-	php -r "if (hash_file('sha384', 'composer-setup.php') === 'ed0feb545ba87161262f2d45a633e34f591ebb3381f2e0063c345ebea4d228dd0043083717770234ec00c5a9f9593792') { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
-	php composer-setup.php
-	php -r "unlink('composer-setup.php');"
-	mv -v composer.phar "$composer"
+	curl -sS https://getcomposer.org/installer | php -- --install-dir="$(dirname "$composer")" --filename=composer
 fi
 # shellcheck disable=SC2154
 slogger -st "$0" "Composer ${green}[OK]${nc}"
