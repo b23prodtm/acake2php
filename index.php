7be3345070427fd98bde3a7b8d0b8d15400e076c
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2025-11-09 23:39:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -29,13 +29,4 @@ define('ROOT', dirname(__FILE__));
 define('WEBROOT_DIR', 'webroot');
 define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
 
-/**
- * This only needs to be changed if the "cake" directory is located
- * outside of the distributed structure.
- * Full path to the directory containing "cake". Do not add trailing directory separator
- */
-if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-	define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
-}
-
 include APP_DIR . DS . WEBROOT_DIR . DS . 'index.php';
