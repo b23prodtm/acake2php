38b7f42683179df40f5a946c8b35f5767c4459e1
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2025-11-09 23:51:36 +0100

    Update paths.php

--- app/Config/paths.php
+++ app/Config/paths.php
@@ -44,7 +44,7 @@ define('APP', ROOT . DS . APP_DIR . DS);
 /*
  * Path to the config directory.
  */
-define('CONFIG', ROOT . DS . 'config' . DS);
+define('CONFIG', APP . DS . 'config' . DS);
 
 /*
  * File path to the webroot directory.
@@ -53,22 +53,22 @@ define('CONFIG', ROOT . DS . 'config' . DS);
  *
  * `define('WWW_ROOT', rtrim($_SERVER['DOCUMENT_ROOT'], DS) . DS);`
  */
-define('WWW_ROOT', ROOT . DS . 'webroot' . DS);
+define('WWW_ROOT', APP . DS . 'webroot' . DS);
 
 /*
  * Path to the tests directory.
  */
-define('TESTS', ROOT . DS . 'tests' . DS);
+define('TESTS', APP . DS . 'tests' . DS);
 
 /*
  * Path to the temporary files directory.
  */
-define('TMP', ROOT . DS . 'tmp' . DS);
+define('TMP', APP . DS . 'tmp' . DS);
 
 /*
  * Path to the logs directory.
  */
-define('LOGS', ROOT . DS . 'logs' . DS);
+define('LOGS', APP . DS . 'logs' . DS);
 
 /*
  * Path to the cache files directory. It can be shared between hosts in a multi-server setup.
@@ -80,7 +80,7 @@ define('CACHE', TMP . 'cache' . DS);
  *
  * CakePHP should always be installed with composer, so look there.
  */
-define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
+define('CAKE_CORE_INCLUDE_PATH', APP . DS . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
 
 /*
  * Path to the cake directory.
