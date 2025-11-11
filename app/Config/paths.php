e224a2ae811f3c4c5090d3b283f46e8262f89d23
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2025-11-11 02:26:55 +0100

    Update paths.php

--- app/Config/paths.php
+++ app/Config/paths.php
@@ -91,3 +91,6 @@ define('CAKE_CORE_INCLUDE_PATH', APP . 'vendor' . DS . 'cakephp' . DS . 'cakephp
  */
 define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
 define('CAKE', CORE_PATH . 'src' . DS);
+
+/* CMS root path */
+define('CMS_ROOT', WWW_ROOT . 'php-cms' . DS . 'e13');
