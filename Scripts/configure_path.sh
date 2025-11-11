df785c9f40b0fbd381aaa799ba51eb05ead8795f
add-MAINTAINER-email-to-environment <add-MAINTAINER-email-to-environment@github.com>
2025-11-11 19:41:18 +0000

    File sessions

--- Scripts/configure_path.sh
+++ Scripts/configure_path.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 TOPDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
-mkdir -p "$TOPDIR/app/tmp/cache/long"
-mkdir -p "$TOPDIR/app/tmp/cache/persistent"
-mkdir -p "$TOPDIR/app/tmp/cache/models"
-mkdir -p "$TOPDIR/app/tmp/tests"
-mkdir -p "$TOPDIR/app/tmp/logs"
-mkdir -p "$TOPDIR/app/logs"
-mkdir -p "$TOPDIR/log"
-chmod -Rv 1770 "$TOPDIR/app/tmp"
-chown -Rv :www-data "$TOPDIR/app/tmp"
-chmod -Rv 1770 "$TOPDIR/app/logs"
-chown -Rv :www-data "$TOPDIR/app/logs"
-chmod -Rv 1770 "$TOPDIR/log"
-chown -Rv :www-data "$TOPDIR/log"
+function create_www_path() {
+  mkdir -p "$1"
+  chmod -Rv 1770 "$1"
+  chown -Rv :www-data "$1"
+}
+create_www_path "$TOPDIR/app/tmp/"
+create_www_path "$TOPDIR/app/tmp/logs"
+create_www_path "$TOPDIR/app/tmp/cache"
+create_www_path "$TOPDIR/app/tmp/cache/long"
+create_www_path "$TOPDIR/app/tmp/cache/persistent"
+create_www_path "$TOPDIR/app/tmp/cache/models"
+create_www_path "$TOPDIR/app/tmp/sessions"
+create_www_path "$TOPDIR/app/logs"
+create_www_path "$TOPDIR/log"
 
