9a5a61c4dc7472fafc40fd4336d4a6774072c0a3
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2026-01-28 08:56:01 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
-if [ ! "$(command -v balena > /dev/null)" ]; then
+if ! command -v balena > /dev/null; then
    printf "%s\n" "balena client not found, install it along with NodeJS 22 (https://nvm.sh)"
    printf "%s\n" "https://github.com/balena-io/balena-cli/blob/master/INSTALL-LINUX.md"
    printf "%s\n" "You must be logged in"
-   printf "%s\n" "[ $(command -v podman) ] && alias docker=podman; docker login"
+   printf "%s\n" "[ \"$\(command -v podman\)\" ] && alias docker=podman; docker login"
    printf "%s\n" "balena login"
 
 fi
-if [ ! "$(command -v balena_deploy > /dev/null)" ]; then
+if ! command -v balena_deploy > /dev/null; then
    printf "%s\n" "balena cloud apps not found, install it"
    printf "%s\n" "sudo npm install -g yarn"
    printf "%s\n" "yarn"
+   printf "%s\n" "sudo npm link balena-cloud-apps"
 fi
 rm -f deployment/images/mysqldb/conf.d/custom.cnf
 # Fixes: unbound variables on ubuntu
