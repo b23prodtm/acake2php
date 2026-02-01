3695c226124b7941c74a7007c27f8453e4162136
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-01 19:09:30 +0100

    armhf pushed ./balena-storage

---
+++ balena-storage/build.armhf.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -eu
+TOPDIR=$(cd "$(dirname $(dirname $(dirname $(dirname "${BASH_SOURCE[0]}"))))" && pwd)
+balena_deploy . armhf 3 0
+docker buildx bake -f docker-bake.hcl balena-storage \
+  --set "*.platform=linux/arm/v7"
