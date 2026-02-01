ac6bf4a9a332df7cb48c5994208944c931d442c7
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2026-02-01 21:42:00 +0100

    Fix TOPDIR variable assignment syntax

--- deployment/images/mysqldb/build.armhf.sh
+++ deployment/images/mysqldb/build.armhf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu
-TOPDIR=$(cd "$(dirname $(dirname $(dirname $(dirname "${BASH_SOURCE[0]}"))))" && pwd)
+TOPDIR="$(cd "$(dirname "$(dirname "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")")" && pwd)"
 ln -s "${TOPDIR}/armhf.env" armhf.env
 balena_deploy "${TOPDIR}" armhf 3 0
 docker buildx bake -f "${TOPDIR}/docker-bake.hcl" db \
