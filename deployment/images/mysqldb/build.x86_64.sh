cf48d6ae3033e32cf57b9cebfa7b804ecf41ed3d
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-01 19:16:18 +0100

    x86_64 pushed ./deployment/images/mysqldb

--- deployment/images/mysqldb/build.x86_64.sh
+++ deployment/images/mysqldb/build.x86_64.sh
@@ -3,4 +3,4 @@ set -eu
 TOPDIR=$(cd "$(dirname $(dirname $(dirname $(dirname "${BASH_SOURCE[0]}"))))" && pwd)
 balena_deploy . x86_64 3 0
 docker buildx bake -f docker-bake.hcl db \
-  --set "*.platform=linux/amd64"
+  --set "*.platform=linux/amd64" --push
