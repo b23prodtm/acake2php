e2fd2bcae9fc43fa454371bb246db55041f8aa4d
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-01 19:08:33 +0100

    aarch64 pushed ./deployment/images/httpd

--- deployment/images/httpd/build.aarch64.sh
+++ deployment/images/httpd/build.aarch64.sh
@@ -2,5 +2,5 @@
 set -eu
 TOPDIR=$(cd "$(dirname $(dirname $(dirname $(dirname "${BASH_SOURCE[0]}"))))" && pwd)
 balena_deploy . aarch64 3 0
-docker buildx bake -f docker-bake.hcl --service httpd \
+docker buildx bake -f docker-bake.hcl httpd \
   --set "*.platform=linux/arm64"
