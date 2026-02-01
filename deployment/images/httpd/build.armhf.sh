55029b361600fae23c86d82d16a18ccfcff77ddf
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-01 19:29:42 +0100

    armhf pushed ./deployment/images/httpd

--- deployment/images/httpd/build.armhf.sh
+++ deployment/images/httpd/build.armhf.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 TOPDIR=$(cd "$(dirname $(dirname $(dirname $(dirname "${BASH_SOURCE[0]}"))))" && pwd)
-balena_deploy . armhf 3 0
-docker buildx bake -f docker-bake.hcl httpd \
+ln -s "${TOPDIR}/armhf.env" armhf.env
+balena_deploy "${TOPDIR}" armhf 3 0
+docker buildx bake -f "${TOPDIR}/docker-bake.hcl" httpd \
   --set "*.platform=linux/arm/v7" --push
