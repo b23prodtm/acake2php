7f2f8cdd407cb074dd85a800f6e10698754da8c9
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-01 19:08:14 +0100

    armhf pushed ./deployment/images/mysqldb

--- deployment/images/mysqldb/build.armhf.sh
+++ deployment/images/mysqldb/build.armhf.sh
@@ -2,5 +2,5 @@
 set -eu
 TOPDIR=$(cd "$(dirname $(dirname $(dirname $(dirname "${BASH_SOURCE[0]}"))))" && pwd)
 balena_deploy . armhf 3 0
-docker buildx bake -f docker-bake.hcl --service mysqldb \
+docker buildx bake -f docker-bake.hcl mysqldb \
   --set "*.platform=linux/arm/v7"
