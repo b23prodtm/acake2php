ff47098488b95639c5d0b1b47918500b4dfcf091
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-01 19:16:18 +0100

    x86_64 pushed ./balena-storage

--- build.x86_64.sh
+++ build.x86_64.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu
-TOPDIR=$(cd "$(dirname $(dirname $(dirname $(dirname "${BASH_SOURCE[0]}"))))" && pwd)
+TOPDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 balena_deploy . x86_64 3 0
 docker buildx bake -f docker-bake.hcl php-fpm \
-  --set "*.platform=linux/amd64"
+  --set "*.platform=linux/amd64" --push
