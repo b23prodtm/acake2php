2f91888ef48df768731178a2b3ad08e2bec194b5
add-MAINTAINER-email-to-environment <add-MAINTAINER-email-to-environment@github.com>
2026-02-01 20:55:23 +0000

    aarch64 pushed ./deployment/images/mysqldb

--- deployment/images/mysqldb/build.aarch64.sh
+++ deployment/images/mysqldb/build.aarch64.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eu
-TOPDIR="$(cd "$(dirname "$(dirname "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")")" && pwd)"
+TOPDIR="$(cd "$(dirname "$(dirname "$(dirname \
+"$(dirname "${BASH_SOURCE[0]}")")")")" && pwd)"
 ln -s "${TOPDIR}/aarch64.env" aarch64.env
 balena_deploy "${TOPDIR}" aarch64 3 0
 docker buildx bake -f "${TOPDIR}/docker-bake.hcl" db \
