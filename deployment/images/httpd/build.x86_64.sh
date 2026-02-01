d2f6a70fd850354a83a434762613f2e7547b8f4f
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-01 19:29:46 +0100

    x86_64 pushed ./deployment/images/httpd

--- deployment/images/httpd/build.x86_64.sh
+++ deployment/images/httpd/build.x86_64.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 TOPDIR=$(cd "$(dirname $(dirname $(dirname $(dirname "${BASH_SOURCE[0]}"))))" && pwd)
-balena_deploy . x86_64 3 0
-docker buildx bake -f docker-bake.hcl httpd \
+ln -s "${TOPDIR}/x86_64.env" x86_64.env
+balena_deploy "${TOPDIR}" x86_64 3 0
+docker buildx bake -f "${TOPDIR}/docker-bake.hcl" httpd \
   --set "*.platform=linux/amd64" --push
