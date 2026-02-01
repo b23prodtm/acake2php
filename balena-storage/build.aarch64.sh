09f78d448506c2bedac2a5bb11cb3eb487ff1d81
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-01 19:16:22 +0100

    aarch64 pushed ./balena-storage

--- balena-storage/build.aarch64.sh
+++ balena-storage/build.aarch64.sh
@@ -3,4 +3,4 @@ set -eu
 TOPDIR=$(cd "$(dirname $(dirname $(dirname $(dirname "${BASH_SOURCE[0]}"))))" && pwd)
 balena_deploy . aarch64 3 0
 docker buildx bake -f docker-bake.hcl balena-storage \
-  --set "*.platform=linux/arm64"
+  --set "*.platform=linux/arm64" --push
--- build.aarch64.sh
+++ build.aarch64.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu
-TOPDIR=$(cd "$(dirname $(dirname $(dirname $(dirname "${BASH_SOURCE[0]}"))))" && pwd)
+TOPDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 balena_deploy . aarch64 3 0
 docker buildx bake -f docker-bake.hcl php-fpm \
-  --set "*.platform=linux/arm64"
+  --set "*.platform=linux/arm64" --push
