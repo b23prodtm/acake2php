91b60749fd46abbd0e3ac3216e7bb0c04aa2cdf5
add-MAINTAINER-email-to-environment <add-MAINTAINER-email-to-environment@github.com>
2026-02-01 20:55:20 +0000

    armhf pushed ./balena-storage

--- balena-storage/build.armhf.sh
+++ balena-storage/build.armhf.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eu
-TOPDIR=$(cd "$(dirname $(dirname $(dirname $(dirname "${BASH_SOURCE[0]}"))))" && pwd)
+TOPDIR="$(cd "$(dirname \
+"$(dirname "${BASH_SOURCE[0]}")")" && pwd)"
 ln -s "${TOPDIR}/armhf.env" armhf.env
 balena_deploy "${TOPDIR}" armhf 3 0
 docker buildx bake -f "${TOPDIR}/docker-bake.hcl" balena-storage \
