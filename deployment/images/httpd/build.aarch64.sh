8c8a6cf2128ce48edfa58a5bfac865058ea33132
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-14 22:01:17 +0100

    aarch64 pushed ./deployment/images/httpd

--- deployment/images/httpd/build.aarch64.sh
+++ deployment/images/httpd/build.aarch64.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 TOPDIR="$(cd "$(dirname "${BASH_SOURCE[0]}"/../../..)" && pwd)"
-ln -s "${TOPDIR}/aarch64.env" aarch64.env
+ln -sf "${TOPDIR}/aarch64.env" aarch64.env
 cp -vf "${TOPDIR}/docker-compose.${BALENA_ARCH}" "${TOPDIR}/docker-compose.yml"
 docker buildx bake -f "${TOPDIR}/docker-bake.hcl" httpd \
   --set "*.platform=linux/arm64" \
