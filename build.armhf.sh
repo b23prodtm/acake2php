71d41b9b43a5f4fa0c38eb700f44fb25d1407545
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-14 21:58:04 +0100

    armhf pushed ./balena-storage

--- build.armhf.sh
+++ build.armhf.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 TOPDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-ln -s "armhf.env" "armhf.env"
+ln -s "armhf.env" ".env"
 cp -vf "${TOPDIR}/docker-compose.armhf" "${TOPDIR}/docker-compose.yml"
 docker buildx bake -f docker-bake.hcl php-fpm \
   --set "*.platform=linux/arm/v7" \
