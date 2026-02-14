81cf831fe3524cfbd2daca0cdc0f591ce824be20
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-14 19:47:47 +0100

    armhf pushed ./balena-storage

--- build.armhf.sh
+++ build.armhf.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 TOPDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-balena_deploy . armhf 3 0
+ln -s "${TOPDIR}/armhf.env" armhf.env
+cp -vf "${TOPDIR}/docker-compose.${BALENA_ARCH}" "${TOPDIR}/docker-compose.yml"
 docker buildx bake -f docker-bake.hcl php-fpm \
-  --set "*.platform=linux/arm/v7" \s
-  --secret id=mysql_user,env=MYSQL_USER \
-  --secret id=mysql_password,env=MYSQL_PASSWORD \
-  --secret id=mysql_root_password,env=MYSQL_ROOT_PASSWORD \
-  --secret id=mysql_database,env=MYSQL_DATABASE \
-  --secret id=master_password,env=MASTER_PASSWORD \
---push
+  --set "*.platform=linux/arm/v7" \
+  --push
