607f1691bcc5ee52850872d82d493583583a44e0
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-14 19:47:58 +0100

    x86_64 pushed ./deployment/images/mysqldb

--- deployment/images/mysqldb/build.x86_64.sh
+++ deployment/images/mysqldb/build.x86_64.sh
@@ -3,6 +3,7 @@ set -eu
 TOPDIR="$(cd "$(dirname "$(dirname "$(dirname \
 "$(dirname "${BASH_SOURCE[0]}")")")")" && pwd)"
 ln -s "${TOPDIR}/x86_64.env" x86_64.env
-balena_deploy "${TOPDIR}" x86_64 3 0
+cp -vf "${TOPDIR}/docker-compose.${BALENA_ARCH}" "${TOPDIR}/docker-compose.yml"
 docker buildx bake -f "${TOPDIR}/docker-bake.hcl" db \
-  --set "*.platform=linux/amd64" --push
+  --set "*.platform=linux/amd64" \
+  --push
