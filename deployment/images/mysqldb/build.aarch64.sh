59a3a81b2a3810b4a7970784d7ae1e0fa9a3f11a
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-14 19:47:54 +0100

    aarch64 pushed ./deployment/images/mysqldb

--- deployment/images/mysqldb/build.aarch64.sh
+++ deployment/images/mysqldb/build.aarch64.sh
@@ -3,6 +3,7 @@ set -eu
 TOPDIR="$(cd "$(dirname "$(dirname "$(dirname \
 "$(dirname "${BASH_SOURCE[0]}")")")")" && pwd)"
 ln -s "${TOPDIR}/aarch64.env" aarch64.env
-balena_deploy "${TOPDIR}" aarch64 3 0
+cp -vf "${TOPDIR}/docker-compose.${BALENA_ARCH}" "${TOPDIR}/docker-compose.yml"
 docker buildx bake -f "${TOPDIR}/docker-bake.hcl" db \
-  --set "*.platform=linux/arm64" --push
+  --set "*.platform=linux/arm64" \
+  --push
