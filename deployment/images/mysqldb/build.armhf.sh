4172ab6a32436fa2a5b28649f700e9a263da01c5
Tina Bruno Rakoto <b23prodtm@users.sourceforge.net>
2026-02-14 19:47:46 +0100

    armhf pushed ./deployment/images/mysqldb

--- deployment/images/mysqldb/build.armhf.sh
+++ deployment/images/mysqldb/build.armhf.sh
@@ -3,6 +3,7 @@ set -eu
 TOPDIR="$(cd "$(dirname "$(dirname "$(dirname \
 "$(dirname "${BASH_SOURCE[0]}")")")")" && pwd)"
 ln -s "${TOPDIR}/armhf.env" armhf.env
-balena_deploy "${TOPDIR}" armhf 3 0
+cp -vf "${TOPDIR}/docker-compose.${BALENA_ARCH}" "${TOPDIR}/docker-compose.yml"
 docker buildx bake -f "${TOPDIR}/docker-bake.hcl" db \
-  --set "*.platform=linux/arm/v7" --push
+  --set "*.platform=linux/arm/v7" \
+  --push
