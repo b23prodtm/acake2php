0385cc4ac06ab45cfa84aeb1ddfb75bbfe6ae817
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2026-02-15 02:05:16 +0100

    Update logging.sh

--- Scripts/lib/logging.sh
+++ Scripts/lib/logging.sh
@@ -3,8 +3,11 @@ TOPDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
 . init_functions .
 # shellcheck source=lib/parsing.sh
 . "${TOPDIR}/Scripts/lib/parsing.sh"
-runner=$(parse_arg "-[rR]+|--runner"  "$@")
-pargs=$(parse_arg_trim "-[rR]+|--runner"  "$@")
+parse_args "$@" <<EOF
+flag runner -r --runner
+end
+EOF
+
 if [ -n "$runner" ]; then
   export CAKEPHP_DEBUG_LEVEL=1
   # shellcheck source=bootargs.sh
